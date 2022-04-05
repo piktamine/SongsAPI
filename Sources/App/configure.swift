70e0e562dbbe5814092922fa50aa6f3df86e5041
piktamine <trujoknow@gmail.com>
2022-04-05 14:29:46 -0500

    unnecessary files

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,7 +16,7 @@ public func configure(_ app: Application) throws {
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
     ), as: .mysql)
 
-    app.migrations.add(CreateTodo())
+    //app.migrations.add(CreateTodo())
 
     app.views.use(.leaf)
 
