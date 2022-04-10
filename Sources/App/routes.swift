67427703a24bf1c258fde785a5ee8c4daf5083ff
piktamine <trujoknow@gmail.com>
2022-04-09 20:00:46 -0500

    more in leaf

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,15 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-        app.get { req in
-            return req.view.render("index", ["title": "Hello Vapor!"])
-        }
+    app.get { req in
+        //let songs = index(req: req)
+        return req.view.render("index", ["title": "Hello Vapor!"])
+    }
     
-        app.get("hello") { req -> String in
-            return "Hello, world!"
-        }
+    
+    app.get("hello") { req -> String in
+        return "Hello, world!"
+    }
     
     //try app.register(collection: TodoController())
 }
